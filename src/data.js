2996f084e1d757504288ae624e745e2aa027565c
Hector <adeniyi.olaitanhector@yahoo.com>
2024-04-08 00:18:01 +0100

    Add wallet data to src/data.js

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const wallet = [
+  {
+    name: "Nami",
+    link: "https://namiwallet.io/",
+  },
+  {
+    name: "Vespr",
+    link: "https://vespr.com/",
+  },
+  {
+    name: "Typhoon",
+    link: "https://typhoon.network/",
+  },
+  {
+    name: "Lace",
+    link: "https://lacewallet.com/",
+  },
+  {
+    name: "Eternl",
+    link: "https://eternl.io/",
+  },
+];
