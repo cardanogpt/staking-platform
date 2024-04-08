965a3db79b9460d4be4be958024dcd07d7da2371
Hector <adeniyi.olaitanhector@yahoo.com>
2024-04-08 02:43:47 +0100

    Add Transaction component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { SideBar } from "./components/SideBar";
 import Nav from "./components/Nav";
 import Dashboard from "./scenes/Dashboard";
+import Transaction from "./components/TransactionHistory";
 
 const drawerWidth = 240;
 
 function App() {
   return (
-    <Box height={"100vh"} sx={{ display: "flex", bgcolor: "secondary.600" }}>
+    <Box
+      minHeight={"100vh"}
+      sx={{ display: "flex", bgcolor: "secondary.600", color: "#ffffff" }}
+    >
       <CssBaseline />
       <Nav title="Dashboard" drawerWidth={drawerWidth} />
       <SideBar />
-      <Dashboard />
+      <Transaction />
     </Box>
   );
 }
