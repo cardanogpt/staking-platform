b0d0da0d0cf23cad9a0f2f7f6e9e732755967419
Hector <adeniyi.olaitanhector@yahoo.com>
2024-04-08 02:43:56 +0100

    Add TransactionData to src/data.js

--- src/data.js
+++ src/data.js
@@ -20,3 +20,55 @@ export const wallet = [
     link: "https://eternl.io/",
   },
 ];
+
+//GENERATE RANDOM DATA FOR TRANSACTION
+export const TransactionData = [
+  {
+    amount: "20 CGI",
+    duration: "3 months",
+    datetime: "2021-10-01T00:00:00Z",
+    status: "Pending",
+  },
+  {
+    amount: "50 CGI",
+    duration: "6 months",
+    datetime: "2021-10-01T00:00:00Z",
+    status: "Pending",
+  },
+  {
+    amount: "100 CGI",
+    duration: "12 months",
+    datetime: "2021-10-01T00:00:00Z",
+    status: "Pending",
+  },
+  {
+    amount: "200 CGI",
+    duration: "24 months",
+    datetime: "2021-10-01T00:00:00Z",
+    status: "Pending",
+  },
+  {
+    amount: "500 CGI",
+    duration: "36 months",
+    datetime: "2021-10-01T00:00:00Z",
+    status: "Pending",
+  },
+  {
+    amount: "2000 CGI",
+    duration: "60 months",
+    datetime: "2021-10-01T00:00:00Z",
+    status: "Executed",
+  },
+  {
+    amount: "5000 CGI",
+    duration: "72 months",
+    datetime: "2021-10-01T00:00:00Z",
+    status: "Pending",
+  },
+  {
+    amount: "10000 CGI",
+    duration: "84 months",
+    datetime: "2021-10-01T00:00:00Z",
+    status: "Pending",
+  },
+];
