a78e3966f4e7400662285d84dd865300a6f0a8a8
Hector <adeniyi.olaitanhector@yahoo.com>
2024-04-10 13:19:15 +0100

    Refactor App.js to add a dashboard layout

--- src/App.js
+++ src/App.js
@@ -11,6 +11,16 @@ function App() {
   return (
     <>
       <ScrollTop />
+      <Box
+        minHeight={"100vh"}
+        width={"100vw"}
+        sx={{ display: "flex", bgcolor: "secondary.600", color: "#ffffff" }}
+      >
+        <CssBaseline />
+        <Nav title="Dashboard" drawerWidth={drawerWidth} />
+        <SideBar />
+        <Outlet />
+      </Box>
     </>
   );
 }
