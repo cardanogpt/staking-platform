0863db3c2b5036d12ee2b6d692f041c80a804a07
Hector <adeniyi.olaitanhector@yahoo.com>
2024-04-10 13:01:42 +0100

    added stake scene

--- src/scenes/Stake.jsx
+++ src/scenes/Stake.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import TransactionHistory from "../components/TransactionHistory";
+import Box from "@mui/material/Box";
+import CardObj from "../components/CardObj";
+import DoubleCardObj from "../components/DoubleCardObj";
+import StakeModal from "../components/StakeModal";
+import CompleteStakeModal from "../components/CompleteStakeModal";
 
 const Stake = () => {
-  return <div>Stake</div>;
+  const [openModal, setModalOpen] = React.useState(false);
+  const handleModalOpen = () => setModalOpen(true);
+  const [completeModalOpen, setCompleteModalOpen] = React.useState(false);
+
+  return (
+    <Box width={"100%"} marginTop={"6rem"}>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="stretch"
+        m="2rem"
+      >
+        <CardObj
+          title="Total Portfolio Value (CGI)"
+          amount="12.33"
+          buttonText1="Stake CGI"
+          buttonText2="Buy CGI Tokens"
+          width={"40%"}
+          handleModalOpen={handleModalOpen}
+        />
+        <DoubleCardObj
+          title1="Total Staked CGI"
+          amount1={0.0}
+          title2="Claimable Token"
+          amount2={0.0}
+          buttonText2="Claim Now"
+          disabled={true}
+          width={"50%"}
+        />
+      </Box>
+      <TransactionHistory />
+      <StakeModal
+        openModal={openModal}
+        setModalOpen={setModalOpen}
+        setCompleteModalOpen={setCompleteModalOpen}
+      />
+      <CompleteStakeModal
+        openModal={completeModalOpen}
+        setModalOpen={setCompleteModalOpen}
+      />
+    </Box>
+  );
 };
 
 export default Stake;
