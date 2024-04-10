698a81069734998a8e55342a04852df151e4c414
Hector <adeniyi.olaitanhector@yahoo.com>
2024-04-10 13:03:09 +0100

    added dashboard scene

--- src/scenes/Dashboard.jsx
+++ src/scenes/Dashboard.jsx
@@ -1,110 +1,45 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import logo from "../assets/images/cardanogpt_logo.png";
-import Modal from "@mui/material/Modal";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import { wallet } from "../data";
-import Link from "@mui/material/Link";
-
-const modalStyle = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 450,
-  bgcolor: "secondary.500",
-  color: "white",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-  backdropFilter: "blur(5px)",
-};
+import CardObj from "../components/CardObj";
+import DoubleCardObj from "../components/DoubleCardObj";
 
 const Dashboard = () => {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
   return (
-    <Box
-      component="main"
-      display={"flex"}
-      width="100%"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ color: "secondary.main", p: 3 }}
-    >
+    <Box width={"100%"} marginTop={"6rem"}>
       <Box
-        display={"flex"}
-        width="20rem"
-        alignItems="center"
-        justifyContent="center"
-        flexDirection="column"
+        display="flex"
+        flexWrap={"wrap"}
+        justifyContent="space-between"
+        alignItems="stretch"
+        rowGap="2rem"
+        m="2rem"
       >
-        <img style={{ width: "5rem", height: "5rem" }} src={logo} alt="" />
-        <Typography variant="h5" color="#FFFFFF">
-          Connect Wallet
-        </Typography>
-        <Typography variant="caption" pb="1rem" textAlign="center">
-          Connect your Cardano wallet to view your Chaincrib dashboard and
-          assets
-        </Typography>
-        <Button
-          sx={{ textTransform: "none" }}
-          variant="contained"
-          onClick={handleOpen}
-        >
-          Connect Wallet
-        </Button>
+        <CardObj
+          title="Total Portfolio Value (CGI)"
+          amount="12.33"
+          buttonText1="Stake CGI"
+          buttonText2="Buy CGI Tokens"
+          width={"40%"}
+        />
+        <CardObj
+          title="Total Portfolio Value (ADA)"
+          amount={252.32}
+          buttonText1="Launch How To Buy ADA"
+          width={"50%"}
+          image={logo}
+          outlined
+        />
+        <DoubleCardObj
+          title1="Total Staked"
+          amount1={52.11}
+          buttonText1="Manage Positions"
+          title2="Claimable Token"
+          amount2={2.01}
+          buttonText2="Claim Now"
+          width={"50%"}
+        />
       </Box>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={modalStyle}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Connect Wallet
-          </Typography>
-          <Typography
-            id="modal-modal-description"
-            variant="caption"
-            sx={{ mt: 2 }}
-          >
-            By connecting your wallet, you agree to the Terms & Conditions and
-            Privacy Policy
-          </Typography>
-          <List>
-            {wallet.map((item) => (
-              <ListItem key={item.name} component={Link} href={item.link}>
-                <ListItemIcon>
-                  <img
-                    style={{ width: "1.5rem", height: "1.5rem" }}
-                    src={`logos/${item.name}.png`}
-                    alt=""
-                  />
-                </ListItemIcon>
-                <ListItemText
-                  sx={{
-                    color: "white",
-                    "& .MuiListItemText-secondary": {
-                      color: "secondary.main",
-                      fontSize: "0.6rem",
-                    },
-                  }}
-                  primary={`${item.name} Wallet`}
-                  secondary={`Connect to your ${item.name} wallet on your browser extension`}
-                />
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-      </Modal>
     </Box>
   );
 };
