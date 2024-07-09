ef30c31db8bc4def9828dd1c405583ba8cc68cdf
Hector <adeniyi.olaitanhector@yahoo.com>
2024-07-09 03:34:43 +0100

    remove APPCacheProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { AppCacheProvider } from "@mui/material-nextjs/v14-pagesRouter";
+/* 
+Removed AppCacheProvider to fix Delayed Styling Bug
+import { AppCacheProvider } from "@mui/material-nextjs/v14-pagesRouter";- 
+
+*/
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "@/theme";
 import Layout from "@/components/Layout";
 import { LucidProvider } from "@/contexts/LucidContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <AppCacheProvider>
-      <LucidProvider>
-        <ThemeProvider theme={theme}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ThemeProvider>
-      </LucidProvider>
-    </AppCacheProvider>
-  );
+	return (
+		<LucidProvider>
+			<ThemeProvider theme={theme}>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</ThemeProvider>
+		</LucidProvider>
+	);
 }
