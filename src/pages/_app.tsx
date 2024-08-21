b3c378b4b3b77cc2c29d66cea7946b92051c9c72
Deniyi <adeniyi.olaitanhector@yahoo.com>
2024-08-21 23:06:11 +0100

    add error provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,15 +9,18 @@ import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "@/theme";
 import Layout from "@/components/Layout";
 import { LucidProvider } from "@/contexts/LucidContext";
+import { ErrorProvider } from "@/contexts/ErrorContext";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<LucidProvider>
-			<ThemeProvider theme={theme}>
-				<Layout>
-					<Component {...pageProps} />
-				</Layout>
-			</ThemeProvider>
-		</LucidProvider>
+		<ErrorProvider>
+			<LucidProvider>
+				<ThemeProvider theme={theme}>
+					<Layout>
+						<Component {...pageProps} />
+					</Layout>
+				</ThemeProvider>
+			</LucidProvider>
+		</ErrorProvider>
 	);
 }
